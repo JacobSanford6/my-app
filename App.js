ec32ea85ec64e469cba1de7584c8a167a8199b20
Jake Sanford <jacob.r.sanford@gmail.com>
2023-01-12 12:56:57 -0600

    First commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js stupid</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFA500 ',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
